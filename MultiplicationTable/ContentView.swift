0f1c8fcc3e50b5cc1ed9ddec6b58d33ab94d0479
Lucas Chae <hi@lucaschae.com>
2022-05-04 19:35:23 +0900

    Initial Commit

---
+++ MultiplicationTable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTable
+//
+//  Created by Lucas Chae on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Lucas Chae on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
